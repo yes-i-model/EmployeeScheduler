166158fa21e462625e7ce05beb1cdbf456f73c13
matthewnebiyou <134652780+matthewnebiyou@users.noreply.github.com>
2023-08-08 16:04:46 -0400

    New main menu options: Modify and Delete Employees
    Added functionality to modify the attributes of existing employees or remove them from the system all together.

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -30,20 +30,53 @@ def add_employee(employees):
     print(f'Employee {name} added successfully!')
     save_data(employees)
 
+def modify_employee(employees):
+    name = input('Enter the name of the employee to modify: ')
+    employee = next((e for e in employees if e['name'] == name), None)
+    if not employee:
+        print(f'Employee {name} not found.')
+        return
+
+    role = input(f'Enter new role for {name} (kitchen or barista): ').strip().lower()
+    employment_time = input(f'Enter new employment time for {name} (full-time or part-time): ').strip().lower()
+    if role not in ['kitchen', 'barista'] or employment_time not in ['full-time', 'part-time']:
+        print('Invalid role or employment time. Please enter valid details.')
+        return
+
+    employee['role'] = role
+    employee['employment_time'] = employment_time
+    print(f'Employee {name} modified successfully!')
+    save_data(employees)
+
+def delete_employee(employees):
+    name = input('Enter the name of the employee to delete: ')
+    employee = next((e for e in employees if e['name'] == name), None)
+    if not employee:
+        print(f'Employee {name} not found.')
+        return
+
+    employees.remove(employee)
+    print(f'Employee {name} deleted successfully!')
+    save_data(employees)
+
 def schedule_shift(shifts, role):
     if not shifts:
         return 'None', 'None'
+    
     morning_shift = shifts.pop(0)
     afternoon_shift = morning_shift
+
     if role == 'barista' and random.random() < 0.8:
         if shifts:
             afternoon_shift = shifts.pop(0)
     elif role == 'kitchen' and random.random() < 0.5:
         if shifts:
             afternoon_shift = shifts.pop(0)
+
     shifts.append(morning_shift)
     if morning_shift != afternoon_shift:
         shifts.append(afternoon_shift)
+
     return morning_shift, afternoon_shift
 
 def view_schedule(employees):
@@ -57,6 +90,7 @@ def view_schedule(employees):
         print(day)
         kitchen_staffer_morning, kitchen_staffer_afternoon = schedule_shift(kitchen_shifts, 'kitchen')
         barista_morning, barista_afternoon = schedule_shift(barista_shifts, 'barista')
+
         print(f"  Kitchen Staffer: {kitchen_staffer_morning} - 8 AM to 12 PM, {kitchen_staffer_afternoon} - 12 PM to 5 PM")
         print(f"  Barista: {barista_morning} - 8 AM to 12 PM, {barista_afternoon} - 12 PM to 5 PM")
 
@@ -71,11 +105,23 @@ def view_schedule(employees):
     for name, hours in scheduled_hours.items():
         print(f"{name}: {hours} hours")
 
+    for employee in employees:
+        if employee['employment_time'] == 'full-time':
+            shifts = [employee['name']] * 5
+        else:
+            shifts = [employee['name']] * 2
+
+        if employee['role'] == 'kitchen':
+            kitchen_shifts.extend(shifts)
+        else:
+            barista_shifts.extend(shifts)
+
+
 def view_employees(employees):
     print('\nList of Employees:')
     for employee in employees:
         print(f"Name: {employee['name']}, Role: {employee['role']}, Employment Time: {employee['employment_time']}")
-    print()
+    print() # Blank line for readability
 
 def main():
     global employees
@@ -85,17 +131,23 @@ def main():
     while True:
         print('\nMenu:')
         print('1. Add Employee')
-        print('2. View Schedule')
-        print('3. View Employees')
-        print('4. Exit')
-        choice = input('Please select an option (1-4): ')
+        print('2. Modify Employee')
+        print('3. Delete Employee')
+        print('4. View Schedule')
+        print('5. View Employees')
+        print('6. Exit')
+        choice = input('Please select an option (1-6): ')
         if choice == '1':
             add_employee(employees)
         elif choice == '2':
-            view_schedule(employees)
+            modify_employee(employees)
         elif choice == '3':
-            view_employees(employees)
+            delete_employee(employees)
         elif choice == '4':
+            view_schedule(employees)
+        elif choice == '5':
+            view_employees(employees)
+        elif choice == '6':
             save_data(employees)
             print('Data saved. Goodbye!')
             break
