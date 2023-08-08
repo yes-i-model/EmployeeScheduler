a90fdd0daaa9c8d307cceecd9172feccbdaa1da8
matthewnebiyou <134652780+matthewnebiyou@users.noreply.github.com>
2023-08-08 15:33:26 -0400

    Data Saved more Frequently
    Added functions to save added employee information more frequently, instead of only when the "Exit" command from the main menu is used.
    
    Now data is saved immediately after an employee is added and when the terminal is closed using the Ctrl+C shortcut

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -1,5 +1,6 @@
 import json
 import random
+import signal
 
 def load_data():
     try:
@@ -12,6 +13,12 @@ def save_data(employees):
     with open('employee_data.json', 'w') as f:
         json.dump(employees, f)
 
+def save_on_terminate(signum, frame):
+    global employees
+    save_data(employees)
+    print("Data saved. Exiting...")
+    exit(0)
+
 def add_employee(employees):
     name = input('Enter the employee name: ')
     role = input('Enter the role (kitchen or barista): ').strip().lower()
@@ -21,25 +28,22 @@ def add_employee(employees):
         return
     employees.append({'name': name, 'role': role, 'employment_time': employment_time})
     print(f'Employee {name} added successfully!')
+    save_data(employees)
 
 def schedule_shift(shifts, role):
     if not shifts:
         return 'None', 'None'
-    
     morning_shift = shifts.pop(0)
     afternoon_shift = morning_shift
-
     if role == 'barista' and random.random() < 0.8:
         if shifts:
             afternoon_shift = shifts.pop(0)
     elif role == 'kitchen' and random.random() < 0.5:
         if shifts:
             afternoon_shift = shifts.pop(0)
-
     shifts.append(morning_shift)
     if morning_shift != afternoon_shift:
         shifts.append(afternoon_shift)
-
     return morning_shift, afternoon_shift
 
 def view_schedule(employees):
@@ -53,7 +57,6 @@ def view_schedule(employees):
         print(day)
         kitchen_staffer_morning, kitchen_staffer_afternoon = schedule_shift(kitchen_shifts, 'kitchen')
         barista_morning, barista_afternoon = schedule_shift(barista_shifts, 'barista')
-
         print(f"  Kitchen Staffer: {kitchen_staffer_morning} - 8 AM to 12 PM, {kitchen_staffer_afternoon} - 12 PM to 5 PM")
         print(f"  Barista: {barista_morning} - 8 AM to 12 PM, {barista_afternoon} - 12 PM to 5 PM")
 
@@ -68,30 +71,22 @@ def view_schedule(employees):
     for name, hours in scheduled_hours.items():
         print(f"{name}: {hours} hours")
 
-    for employee in employees:
-        if employee['employment_time'] == 'full-time':
-            shifts = [employee['name']] * 5
-        else:
-            shifts = [employee['name']] * 2
-
-        if employee['role'] == 'kitchen':
-            kitchen_shifts.extend(shifts)
-        else:
-            barista_shifts.extend(shifts)
-
 def view_employees(employees):
     print('\nList of Employees:')
     for employee in employees:
         print(f"Name: {employee['name']}, Role: {employee['role']}, Employment Time: {employee['employment_time']}")
-    print() # Blank line for readability
+    print()
 
 def main():
+    global employees
     employees = load_data()
+    signal.signal(signal.SIGINT, save_on_terminate)
+
     while True:
         print('\nMenu:')
         print('1. Add Employee')
         print('2. View Schedule')
-        print('3. View Employees')  # New option to view employees
+        print('3. View Employees')
         print('4. Exit')
         choice = input('Please select an option (1-4): ')
         if choice == '1':
@@ -99,7 +94,7 @@ def main():
         elif choice == '2':
             view_schedule(employees)
         elif choice == '3':
-            view_employees(employees)  # New function call to view employees
+            view_employees(employees)
         elif choice == '4':
             save_data(employees)
             print('Data saved. Goodbye!')
