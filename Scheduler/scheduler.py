8863a766ed604e8805f9a4259bcd0b1e2b9fb80f
matthewnebiyou <134652780+matthewnebiyou@users.noreply.github.com>
2023-08-10 13:21:31 -0400

    Consolidated Employee-Related Menu Items & Improved Schedule Readability
    Consolidated the three employee related main menu items (add employee, modify employee and delete employee) into one "Edit Employee" menu item
    
    Also incorporated ASCII art into schedule to make it more readable

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -80,19 +80,24 @@ def schedule_shift(shifts, role):
     return morning_shift, afternoon_shift
 
 def view_schedule(employees):
-    print('\nShift Schedule:')
+    print('\n' + '-'*40)
+    print('Shift Schedule'.center(40))
+    print('-'*40)
+
     days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     kitchen_shifts = [employee['name'] for employee in employees if employee['role'] == 'kitchen']
     barista_shifts = [employee['name'] for employee in employees if employee['role'] == 'barista']
     scheduled_hours = {employee['name']: 0 for employee in employees}
 
     for day in days_of_week:
-        print(day)
+        print('\n' + day.center(40, '-'))
         kitchen_staffer_morning, kitchen_staffer_afternoon = schedule_shift(kitchen_shifts, 'kitchen')
         barista_morning, barista_afternoon = schedule_shift(barista_shifts, 'barista')
 
-        print(f"  Kitchen Staffer: {kitchen_staffer_morning} - 8 AM to 12 PM, {kitchen_staffer_afternoon} - 12 PM to 5 PM")
-        print(f"  Barista: {barista_morning} - 8 AM to 12 PM, {barista_afternoon} - 12 PM to 5 PM")
+        print(f"  Kitchen: {kitchen_staffer_morning:15} 8 AM - 12 PM")
+        print(f"          {kitchen_staffer_afternoon:15} 12 PM - 5 PM")
+        print(f"  Barista: {barista_morning:15} 8 AM - 12 PM")
+        print(f"          {barista_afternoon:15} 12 PM - 5 PM")
 
         for staffer in [kitchen_staffer_morning, kitchen_staffer_afternoon, barista_morning, barista_afternoon]:
             if staffer != 'None':
@@ -101,9 +106,11 @@ def view_schedule(employees):
                 if staffer == kitchen_staffer_afternoon or staffer == barista_afternoon:
                     scheduled_hours[staffer] += 5
 
-    print('\nEmployee Scheduled Hours:')
+    print('\n' + '-'*40)
+    print('Employee Scheduled Hours'.center(40))
+    print('-'*40)
     for name, hours in scheduled_hours.items():
-        print(f"{name}: {hours} hours")
+        print(f"{name:20}: {hours} hours")
 
     for employee in employees:
         if employee['employment_time'] == 'full-time':
@@ -115,7 +122,6 @@ def view_schedule(employees):
             kitchen_shifts.extend(shifts)
         else:
             barista_shifts.extend(shifts)
-
 
 def view_employees(employees):
     print('\nList of Employees:')
@@ -130,24 +136,31 @@ def main():
 
     while True:
         print('\nMenu:')
-        print('1. Add Employee')
-        print('2. Modify Employee')
-        print('3. Delete Employee')
-        print('4. View Schedule')
-        print('5. View Employees')
-        print('6. Exit')
-        choice = input('Please select an option (1-6): ')
+        print('1. Edit Employees')
+        print('2. View Schedule')
+        print('3. View Employees')
+        print('4. Exit')
+        choice = input('Please select an option (1-4): ')
+
         if choice == '1':
-            add_employee(employees)
+            print('\nEdit Employees:')
+            print('a. Add Employee')
+            print('b. Modify Employee')
+            print('c. Delete Employee')
+            edit_choice = input('Please select an option (a-c): ')
+            if edit_choice == 'a':
+                add_employee(employees)
+            elif edit_choice == 'b':
+                modify_employee(employees)
+            elif edit_choice == 'c':
+                delete_employee(employees)
+            else:
+                print('Invalid option, please try again.')
         elif choice == '2':
-            modify_employee(employees)
-        elif choice == '3':
-            delete_employee(employees)
-        elif choice == '4':
             view_schedule(employees)
-        elif choice == '5':
+        elif choice == '3':
             view_employees(employees)
-        elif choice == '6':
+        elif choice == '4':
             save_data(employees)
             print('Data saved. Goodbye!')
             break
